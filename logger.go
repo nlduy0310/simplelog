858e32be879a44dd140cd79fb19ab667bbdbe949
Duy Nguyen Le <nlduy0310@gmail.com>
2026-04-03 20:58:20 +0700

    add support for raw string logging

--- logger.go
+++ logger.go
@@ -31,36 +31,76 @@ func (l *Logger) SetLevel(level Level) {
 	l.level = level
 }
 
-func (l *Logger) Debug(format string, args ...any) {
-	l.logAt(DEBUG, format, args...)
+func (l *Logger) allow(level Level) bool {
+	return l.level.Allow(level)
 }
 
-func (l *Logger) Info(format string, args ...any) {
-	l.logAt(INFO, format, args...)
+// format-style log
+
+func (l *Logger) Debugf(format string, args ...any) {
+	l.logfIfNeeded(DEBUG, format, args...)
 }
 
-func (l *Logger) Warning(format string, args ...any) {
-	l.logAt(WARNING, format, args...)
+func (l *Logger) Infof(format string, args ...any) {
+	l.logfIfNeeded(INFO, format, args...)
 }
 
-func (l *Logger) Error(format string, args ...any) {
-	l.logAt(ERROR, format, args...)
+func (l *Logger) Warningf(format string, args ...any) {
+	l.logfIfNeeded(WARNING, format, args...)
 }
 
-// Fatal logs at level FATAL then calls os.Exit(1)
-func (l *Logger) Fatal(format string, args ...any) {
-	l.logAt(FATAL, format, args...)
+func (l *Logger) Errorf(format string, args ...any) {
+	l.logfIfNeeded(ERROR, format, args...)
+}
+
+// Fatalf logs at level FATAL then calls os.Exit(1)
+func (l *Logger) Fatalf(format string, args ...any) {
+	l.logfIfNeeded(FATAL, format, args...)
 	os.Exit(1)
 }
 
-func (l *Logger) logAt(level Level, format string, args ...any) {
-	if l.level.Allow(level) {
-		l.logger.Printf(
-			"%s - [%s] [%s]: %s\n",
-			time.Now().Format(timestampFormat),
-			l.Name,
-			level.Name(),
-			fmt.Sprintf(format, args...),
-		)
+// log literal strings
+
+func (l *Logger) Debug(msg string) {
+	l.logIfNeeded(DEBUG, msg)
+}
+
+func (l *Logger) Info(msg string) {
+	l.logIfNeeded(INFO, msg)
+}
+
+func (l *Logger) Warning(msg string) {
+	l.logIfNeeded(WARNING, msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.logIfNeeded(ERROR, msg)
+}
+
+// Fatal logs the message then calls os.Exit(1)
+func (l *Logger) Fatal(msg string) {
+	l.logIfNeeded(FATAL, msg)
+	os.Exit(1)
+}
+
+func (l *Logger) logfIfNeeded(level Level, format string, args ...any) {
+	if l.allow(level) {
+		l.logAt(level, fmt.Sprintf(format, args...))
 	}
+}
+
+func (l *Logger) logIfNeeded(level Level, msg string) {
+	if l.allow(level) {
+		l.logAt(level, msg)
+	}
+}
+
+func (l *Logger) logAt(level Level, msg string) {
+	l.logger.Printf(
+		"%s - [%s] [%s]: %s\n",
+		time.Now().Format(timestampFormat),
+		l.Name,
+		level.Name(),
+		msg,
+	)
 }
